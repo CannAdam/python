70735bcf6481097553556de544991e1afbf3a704
Adam Cann <ajcann98@gmail.com>
2022-02-25 20:30:09 -0500

    enter

---
+++ Variables.py
@@ -0,0 +1,11 @@
+#variables are just nicknames for code
+#plain lowercase words
+# examples: bannana_quail
+
+name = "Adam Cann"
+orphan_fee = 200
+teddy_bear_fee = 121.80
+
+total = orphan_fee + teddy_bear_fee
+
+print(name, "the total will be", total)
---
+++ import random.py
@@ -0,0 +1,10 @@
+import random
+
+	bars = ["mad hatter", "twistys", "hideaway"]
+
+	people = ["scott", "maile", "chuck"]
+
+	random_bar = random.choice(bars)
+	random_people = random.choice(people)
+
+	print(f"How would you like to go to {random_bar} with {random_people}?)
